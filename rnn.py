72334dd1297cf6528f909d0abbcce5d12fc1ff01
Aishwarya Kulkarni <skay4kulkarni@gmail.com>
2020-03-14 11:08:36 +0530

    Add files via upload

---
+++ rnn.py
@@ -0,0 +1,99 @@
+from __future__ import print_function
+
+import os
+os.environ["THEANO_FLAGS"] = "mode=FAST_RUN,device=gpu,floatX=float32"
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas
+import math
+
+
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from sklearn.preprocessing import MinMaxScaler
+
+plt.style.use('ggplot')
+np.random.seed(10)
+dataframe = pandas.read_csv('/home/aishwaryak/Desktop/gwvaluesonly.csv', usecols=[1], engine='python', skipfooter=3)
+dataset   = dataframe.values
+dataset   = dataset.astype('float32')
+dataframe.head()
+plt.plot(dataset)
+scaler  = MinMaxScaler(feature_range=(0, 1))
+dataset = scaler.fit_transform(dataset)
+print(dataframe.isnull().sum())
+print(dataframe.isnull())
+
+# split into train and test sets
+train_size  = int(len(dataset) * 0.70)
+test_size   = len(dataset) - train_size
+train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
+print(len(train), len(test))
+print('----------------- TRAINING DATA -----------------')
+plt.plot(train)
+plt.show()
+print('----------------- TEST DATA -----------------')
+plt.plot(test)
+plt.show()
+# This function creates a sliding window of the dataset.
+def create_dataset(dataset, sliding_window=10):
+    dataX, dataY = [], []
+    for i in range(len(dataset)-sliding_window-1):
+        a = dataset[i:(i+sliding_window), 0]
+        dataX.append(a)
+        dataY.append(dataset[i + sliding_window, 0])
+    return np.array(dataX), np.array(dataY)
+# use a n-10 sliding window equivalent to 2.5 hours of historical data
+slide_window   = 1
+trainX, trainY = create_dataset(train, slide_window)
+testX, testY   = create_dataset(test, slide_window)
+trainX = np.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
+testX  = np.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
+#Setup the LSTM
+
+model = Sequential()
+model.add(LSTM(units=4, input_dim=slide_window))
+model.add(Dense(1))
+model.compile(loss='mean_squared_error', optimizer='adam')
+model.fit(trainX, trainY, nb_epoch=1000, batch_size=5, verbose=1)
+
+
+# Print out the evaluation for both the
+trainScore = model.evaluate(trainX, trainY, verbose=0)
+trainScore = math.sqrt(trainScore)
+trainScore = scaler.inverse_transform(np.array([[trainScore]]))
+print('Train Score: %.2f RMSE' % (trainScore))
+testScore = model.evaluate(testX, testY, verbose=0)
+testScore = math.sqrt(testScore)
+testScore = scaler.inverse_transform(np.array([[testScore]]))
+print('Test Score: %.2f RMSE' % (testScore))
+trainPredict = model.predict(trainX)
+testPredict  = model.predict(testX)
+ 
+# shift train predictions for plotting
+trainPredictPlot       = np.empty_like(dataset)
+trainPredictPlot[:, :] = np.nan
+trainPredictPlot[slide_window:len(trainPredict)+slide_window, :] = trainPredict
+ 
+# shift test predictions for plotting
+testPredictPlot       = np.empty_like(dataset)
+testPredictPlot[:, :] = np.nan
+testPredictPlot[len(trainPredict)+(slide_window*2)+1:len(dataset)-1, :] = testPredict
+ 
+# plot baseline and predictions
+plt.plot(dataset)
+plt.plot(trainPredictPlot)
+plt.plot(testPredictPlot)
+plt.show()
+plt.plot(testPredict)
+plt.plot(testY)
+
+
+
+
+
+
+
